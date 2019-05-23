eec6523bc80169142de4075c8067588654744369
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-24 10:40:41 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
-# Create directories and links for mount points etc.
+# Create directories
 mkdir -p /config /downloads
+
+# Link scripts
 ln -s /usr/local/bin/python2.7 /usr/bin/python
 ln -s /usr/local/bin/python2.7 /usr/bin/python2
 
-# Add media user
+# Add System user
 pw group add -n Admin -g 1000
 pw user add System -u 1000 -g 1000 -d /nonexistent -s /usr/bin/nologin
-
-# Ensure the rc.d folder exists
-mkdir -p /usr/local/etc/rc.d
-
-# Copy the nzbget file so we can use 'service nzbget start|stop|etc'
-cp ./nzbget.rc  /mnt/iocage/jails/nzbget/root/etc/rc.d/nzbget
 
 # Set permissions
 chmod 555 /etc/rc.d/nzbget
 
-# Set permissions on mounted folders 
-chown -R media:media /mnt/transfer /mnt/config /usr/local/share/nzbget
+# Set permissions
+chown -R Admin:System /usr/local/share/nzbget
+sysrc nzbget_user=System
+sysrc nzbget_group=Admin
+
+# Set config directory
+sysrc nzbget_conf_dir=/config
 
 # Enable nzbget
-sysrc nzbget_user=media
 sysrc nzbget_enable=YES
-sysrc nzbget_conf_dir=/config
