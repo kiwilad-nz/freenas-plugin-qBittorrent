c2217ee819ddd749e96b37add32cc9fcc84cd3d2
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-24 10:56:14 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -7,19 +7,19 @@ mkdir -p /config /downloads
 ln -s /usr/local/bin/python2.7 /usr/bin/python
 ln -s /usr/local/bin/python2.7 /usr/bin/python2
 
-# Add System user
+# Add system user
 pw group add -n Admin -g 1000
 pw user add System -u 1000 -g 1000 -d /nonexistent -s /usr/bin/nologin
 
 # Set permissions
-chmod 555 /etc/rc.d/nzbget
+# chmod 555 /etc/rc.d/nzbget
 
-# Set permissions
+# Set permissions and enable service to user/group
 chown -R Admin:System /usr/local/share/nzbget
 sysrc nzbget_user=System
 sysrc nzbget_group=Admin
 
-# Download config and set config directory
+# Download/replace config and set config directory
 rm /usr/local/etc/nzbget.conf
 wget -P /usr/local/etc/ https://raw.githubusercontent.com/kiwilad-nz/freenas-plugin-NZBget/master/nzbget.conf
 sysrc nzbget_conf_dir=/config
