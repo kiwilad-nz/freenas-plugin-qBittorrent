f2fb4bfaf0cbb50976a246501ec17aa3763f22c4
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-24 14:27:26 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,13 +2,13 @@
 
 # Create directories
 mkdir -p /config /downloads
-chown -R System:Admin /config /downloads
 
 # Add system user
 pw group add -n Admin -g 1000
 pw user add System -u 1000 -g 1000 -d /nonexistent -s /usr/bin/nologin
 
 # Set permissions and enable service to user/group
+chown -R System:Admin /config /downloads
 sysrc qbittorrent_user=System
 sysrc qbittorrent_group=Admin
 
