e96ada53cbedbb7bc535ada40ead894a3f41cd29
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-26 00:19:59 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,8 +3,8 @@
 # Create directories
 mkdir -p /config /downloads
 
-# Modify installed user qbitorrent (989:989) tp match constant UID/GID from FreeNAS (1000:1000)
-pw groupmod qbittorrent -n Admin -g 1000
+# Modify installed user qbitorrent (989:989) tp match System:Admin UID:GID from FreeNAS (1000:1000)
+pw groupmod -n qbittorrent -l Admin -g 1000
 pw usermod -n qbittorrent -l System -u 1000 -g 1000
 
 # Set permissions and enable service to user/group
