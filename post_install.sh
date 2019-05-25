2a911c16733a7d9db2dfbc1919cb2ec3f2abf740
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-25 19:20:10 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -16,7 +16,7 @@ sysrc qbittorrent_conf_dir=/config
 
 # Enable qbittorrent
 sysrc qbittorrent_enable=YES
-service qbittorrent restart
+service qbittorrent start 2>/dev/null
 
 # Complete message - Print below text
 echo "Please open the URL to continue to Configure qBittorrent (user: admin, Password: adminadmin)."
