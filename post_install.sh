c66af25cf99509110f3b2cb3ba06f115921a7c4c
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-25 14:48:35 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -8,7 +8,6 @@ pw group add -n Admin -g 1000
 pw user add System -u 1000 -g 1000 -d /nonexistent -s /usr/bin/nologin
 
 # Set permissions and enable service to user/group
-chown -R System:Admin /config /downloads
 sysrc qbittorrent_user=System
 sysrc qbittorrent_group=Admin
 
@@ -17,3 +16,5 @@ sysrc qbittorrent_conf_dir=/config
 
 # Enable qbittorrent
 sysrc qbittorrent_enable=YES
+
+echo "Please open the URL and Login with Username: admin, Password: adminadmin"
