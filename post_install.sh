c031241102a3e27b3be681911bdb6832cdbcaa15
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-28 03:46:07 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -7,18 +7,16 @@ mkdir -p /config /downloads
 pw groupadd -n media -g 8675309
 pw useradd -n system -u 1000 -g 8675309 -d /nonexistent -s /usr/sbin/nologin
 
-# Set config directory
-sysrc -f /etc/rc.conf qbittorrent_conf_dir="/config"
-
-# Set service to run using the user:group
-sysrc -f /etc/rc.conf qbittorrent_user="system"
-sysrc -f /etc/rc.conf qbittorrent_group="media"
-
 # Set folder/file permissions to the user:group
 chown -R system:media /var/db/qbittorrent/ /config /downloads
 
-# Enable qbittorrent
+# Enable service and set service to run using specified user/group
+sysrc -f /etc/rc.conf qbittorrent_user="system"
+sysrc -f /etc/rc.conf qbittorrent_group="media"
 sysrc -f /etc/rc.conf qbittorrent_enable="YES"
+
+# Set config directory
+sysrc -f /etc/rc.conf qbittorrent_conf_dir="/config"
 
 # Set downloads folder
 sysrc -f /etc/rc.conf qbittorrent_download_dir="/downloads"
