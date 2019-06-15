7e60b7a7c38f71e3d604d09e2f61612d0926afbb
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-06-15 17:56:05 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -21,7 +21,7 @@ sysrc -f /etc/rc.conf qbittorrent_conf_dir="/config"
 # Set downloads folder
 sysrc -f /etc/rc.conf qbittorrent_download_dir="/downloads"
 
-# Enable Port 80 and redirect to Emby port 8096
+# Enable Port 80 and redirect to qBittorrent port 8080
 echo "net.inet.IP.portrange.reservedhigh=79" >> /etc/sysctl.conf
 echo "firewall_enable=YES" >> /etc/rc.conf
 echo "ipfw add 100 fwd 127.0.0.1,8080 tcp from any to any 80 in" >> /etc/rc.firewall
